b935f1bbef73ec7da9cd38ffa917ab05ba23fdc1
Sezai YUKSEL <sezaiyuksel02@gmail.com>
2023-02-11 00:20:24 +0300

    feat: auth page flow updates

--- lib/resources/pages/auth_page.dart
+++ lib/resources/pages/auth_page.dart
@@ -1,36 +1,49 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/resources/pages/home_page.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 import '/app/controllers/controller.dart';
 
 class AuthPage extends NyStatefulWidget {
+  static const path = '/auth';
+
   final Controller controller = Controller();
   AuthPage({Key? key}) : super(key: key);
-  
+
   @override
   _AuthPageState createState() => _AuthPageState();
 }
 
 class _AuthPageState extends NyState<AuthPage> {
-
   @override
   init() async {
     super.init();
-    
   }
-  
+
   @override
   void dispose() {
     super.dispose();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
-      body: SafeArea(
-         child: Container(),
+      appBar: AppBar(),
+      body: SignInScreen(
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            routeTo(
+              HomePage.path,
+              removeUntilPredicate: (route) => false,
+            );
+          }),
+          AuthStateChangeAction<UserCreated>((context, state) {
+            routeTo(
+              HomePage.path,
+              removeUntilPredicate: (route) => false,
+            );
+          }),
+        ],
       ),
     );
   }
