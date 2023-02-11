1bfef46058ac8a067912c37b4119326c3432de83
Yunus Emre <4984548+yunusemrecetin@users.noreply.github.com>
2023-02-11 16:53:08 +0300

    fix(theme): dark and light mode based color problem solved (#59)

--- lib/resources/themes/styles/dark_theme_colors.dart
+++ lib/resources/themes/styles/dark_theme_colors.dart
@@ -8,31 +8,48 @@ import '/resources/themes/styles/color_styles.dart';
 */
 
 class DarkThemeColors implements ColorStyles {
-  // general
-  Color get background => const Color(0xFF232c33);
+  //*  Guideline Color Scheme (Light) Colors
+  Color get white => Color(0xFFFFFFFF);
+  Color get background => Color(0xFFF9FAFB);
+  Color get tableContext => Color(0xFF101828);
+  Color get title => Color(0xFF101828);
+  Color get context => Color(0xFF223A52);
+  Color get contextAlternative => Color(0xFF475467);
+  MaterialColor get grey => MaterialColor(0xFFDBDBDB, <int, Color>{
+        50: Color(0xFFFEFEFE),
+        100: Color(0xFFFBFBFB),
+        200: Color(0xFFF7F7F7),
+        300: Color(0xFFF4F4F4),
+        400: Color(0xFF98A2B3),
+        500: Color(0xFFECECEC),
+        600: Color(0xFFE9E9E9),
+        700: Color(0xFFE5E5E5),
+        800: Color(0xFFE2E2E2),
+        900: Color(0xFFDEDEDE),
+      });
 
-  Color get primaryContent => const Color(0xFFE1E1E1);
-  Color get primaryAccent => const Color(0xFF9999aa);
+  Color get primaryContent => const Color(0xFF000000);
+  Color get primaryAccent => const Color(0xFF87c694);
 
-  Color get surfaceBackground => Colors.white70;
+  Color get surfaceBackground => Colors.white;
   Color get surfaceContent => Colors.black;
 
   // app bar
-  Color get appBarBackground => const Color(0xFF4b5e6d);
+  Color get appBarBackground => Colors.blue;
   Color get appBarPrimaryContent => Colors.white;
 
   // buttons
-  Color get buttonBackground => Colors.white60;
-  Color get buttonPrimaryContent => const Color(0xFF232c33);
+  Color get buttonBackground => Colors.blueAccent;
+  Color get buttonPrimaryContent => Colors.white;
 
   // bottom tab bar
-  Color get bottomTabBarBackground => const Color(0xFF232c33);
+  Color get bottomTabBarBackground => Colors.white;
 
   // bottom tab bar - icons
-  Color get bottomTabBarIconSelected => Colors.white70;
-  Color get bottomTabBarIconUnselected => Colors.white60;
+  Color get bottomTabBarIconSelected => Colors.blue;
+  Color get bottomTabBarIconUnselected => Colors.black54;
 
   // bottom tab bar - label
-  Color get bottomTabBarLabelUnselected => Colors.white54;
-  Color get bottomTabBarLabelSelected => Colors.white;
+  Color get bottomTabBarLabelUnselected => Colors.black45;
+  Color get bottomTabBarLabelSelected => Colors.black;
 }
