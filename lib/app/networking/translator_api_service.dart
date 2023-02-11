f1acf4c294adde95e7a0c237a9efc7bef7ebfd73
Yunus Emre <4984548+yunusemrecetin@users.noreply.github.com>
2023-02-11 17:44:19 +0300

    #13 (#61)
    * fix(theme): dark and light mode based color problem solved
    
    * feat(api-service): crud features are completed

--- lib/app/networking/translator_api_service.dart
+++ lib/app/networking/translator_api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app/models/translator.dart';
@@ -19,11 +21,157 @@ class TranslatorApiService extends BaseApiService {
       var collection = await FirebaseFirestore.instance
           .collection(getEnv('TRANSLATOR_DB'))
           .get();
-          
+
       collection.docs
           .forEach((element) => list.add(Translator.fromJson(element.data())));
     } catch (e) {}
 
     return list;
+  }
+
+  Future<String?> insert(Translator model) async {
+    try {
+      var collection =
+          await FirebaseFirestore.instance.collection(getEnv('TRANSLATOR_DB'));
+
+      DocumentReference data = await collection.add(model.toJson());
+
+      return data.id;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<bool> update(String docId, Translator model) async {
+    var collection =
+        await FirebaseFirestore.instance.collection(getEnv('TRANSLATOR_DB'));
+
+    try {
+      await collection.doc(docId).update(model.toJson());
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> delete(String docId) async {
+    var collection =
+        await FirebaseFirestore.instance.collection(getEnv('TRANSLATOR_DB'));
+
+    try {
+      await collection.doc(docId).delete();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<Translator?> findOne({
+    String? name,
+    String? phone,
+    String? email,
+    String? facebook,
+    String? instagram,
+    String? messenger,
+    String? language,
+  }) async {
+    var collection =
+        await FirebaseFirestore.instance.collection(getEnv('TRANSLATOR_DB'));
+
+    var findOne = collection;
+
+    if (null != name && name.isNotEmpty) {
+      findOne.where("name", isEqualTo: name);
+    }
+
+    if (null != phone && phone.isNotEmpty) {
+      findOne.where("phone", isEqualTo: phone);
+    }
+
+    if (null != email && email.isNotEmpty) {
+      findOne.where("email", isEqualTo: email);
+    }
+
+    if (null != facebook && facebook.isNotEmpty) {
+      findOne.where("facebook", isEqualTo: facebook);
+    }
+
+    if (null != instagram && instagram.isNotEmpty) {
+      findOne.where("instagram", isEqualTo: instagram);
+    }
+
+    if (null != messenger && messenger.isNotEmpty) {
+      findOne.where("messenger", isEqualTo: messenger);
+    }
+
+    if (null != language && language.isNotEmpty) {
+      findOne.where("languages", arrayContains: language);
+    }
+
+    findOne.limit(1);
+
+    try {
+      var results = await findOne.get();
+      return Translator.fromJson(results.docs.first.data());
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future<List<Translator>> find({
+    String? name,
+    String? phone,
+    String? email,
+    String? facebook,
+    String? instagram,
+    String? messenger,
+    String? language,
+    int? limits,
+  }) async {
+    var collection =
+        await FirebaseFirestore.instance.collection(getEnv('TRANSLATOR_DB'));
+
+    var findOne = collection;
+
+    if (null != name && name.isNotEmpty) {
+      findOne.where("name", isEqualTo: name);
+    }
+
+    if (null != phone && phone.isNotEmpty) {
+      findOne.where("phone", isEqualTo: phone);
+    }
+
+    if (null != email && email.isNotEmpty) {
+      findOne.where("email", isEqualTo: email);
+    }
+
+    if (null != facebook && facebook.isNotEmpty) {
+      findOne.where("facebook", isEqualTo: facebook);
+    }
+
+    if (null != instagram && instagram.isNotEmpty) {
+      findOne.where("instagram", isEqualTo: instagram);
+    }
+
+    if (null != messenger && messenger.isNotEmpty) {
+      findOne.where("messenger", isEqualTo: messenger);
+    }
+
+    if (null != language && language.isNotEmpty) {
+      findOne.where("languages", arrayContains: language);
+    }
+
+    if (null != limits && limits > 0) {
+      findOne.limit(limits);
+    }
+
+    try {
+      var results = await findOne.get();
+      return results.docs
+          .map((element) => Translator.fromJson(element.data()))
+          .toList();
+    } catch (e) {
+      return [];
+    }
   }
 }
