cec77e37a9f762e162d0fcd198dd6b2f75cbff93
Samed Ozdemir <samed@xsor.io>
2023-02-12 14:53:25 -0500

    feat: Small UI Updates

--- lib/app/providers/app_provider.dart
+++ lib/app/providers/app_provider.dart
@@ -18,7 +18,7 @@ class AppProvider implements NyProvider {
         assetsDirectory: assetsDirectory,
         valuesAsMap: valuesAsMap);
 
-    nylo.initialRoute = AuthService().hasSession ? HomePage.path : AuthPage.path;
+    nylo.initialRoute = AuthService().hasSession ? HomePage.path : SignInPage.path;
     nylo.appLoader = loader;
     nylo.appLogo = logo;
     nylo.appThemes = appThemes;
