d7103732bc05e0f8bbc52e10557437da39640f46
Samed Ozdemir <samed@xsor.io>
2023-02-15 21:05:08 -0500

    fix: Remove unused pages

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_app/resources/pages/components_demo_page.dart';
-import 'package:flutter_app/resources/pages/enter_verification_code_page.dart';
 import 'package:flutter_app/resources/pages/home_page.dart';
 import 'package:flutter_app/resources/pages/translator_list_page.dart';
 import 'package:flutter_app/resources/pages/translator_profile_page.dart';
@@ -23,8 +22,6 @@ appRouter() => nyRoutes((router) {
       router.route(HomePage.path, (context) => HomePage());
       router.route(AuthPage.path, (context) => AuthPage());
       router.route(ComponentsDemoPage.path, (context) => ComponentsDemoPage());
-      router.route(EnterVerificationCodePage.path,
-          (context) => EnterVerificationCodePage());
       router.route(
           BecomeTranslatorPage.path, (context) => BecomeTranslatorPage());
       router.route(
