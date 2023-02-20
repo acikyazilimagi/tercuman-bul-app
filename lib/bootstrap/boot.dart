8e2df2f593610074e50e471028b132073f80c087
Samed Ozdemir <samed@xsor.io>
2023-02-19 22:35:58 -0500

    fix: Hack dotenv loading

--- lib/bootstrap/boot.dart
+++ lib/bootstrap/boot.dart
@@ -1,8 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import '../config/providers.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
 /// Boot methods for Nylo.
 class Boot {
-  static Future<Nylo> nylo() async => await bootApplication(providers);
+  static Future<Nylo> nylo() async {
+    const envPath = String.fromEnvironment('ENV_FILE_PATH', defaultValue: 'dotenv2');
+    await dotenv.load(fileName: envPath);
+    return await bootApplication(providers);
+  }
   static Future<void> finished(Nylo nylo) async => await bootFinished(nylo);
 }
