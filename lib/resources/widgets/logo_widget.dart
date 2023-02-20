986f3f8550578d114feb85dc81afaa0c88d0c152
Samed Ozdemir <samed@xsor.io>
2023-02-19 22:30:35 -0500

    chore: Cleanup

--- lib/resources/widgets/logo_widget.dart
+++ lib/resources/widgets/logo_widget.dart
@@ -11,7 +11,7 @@ class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      getImageAsset("images/nylo_logo.png"),
+      getImageAsset("images/logo_horiz.png"),
       height: height ?? 100,
       width: width ?? 100,
     );
