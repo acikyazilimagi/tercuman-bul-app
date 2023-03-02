1d554368e6262040d292de613317f9d3a50aff6d
Samed Ozdemir <samed@xsor.io>
2023-03-01 20:54:04 -0500

    fix: Update Nylo (env file loading fix)

--- lib/bootstrap/boot.dart
+++ lib/bootstrap/boot.dart
@@ -1,14 +1,8 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import '../config/providers.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
 /// Boot methods for Nylo.
 class Boot {
-  static Future<Nylo> nylo() async {
-    const envPath = String.fromEnvironment('ENV_FILE_PATH', defaultValue: 'dotenv');
-    await dotenv.load(fileName: envPath);
-    return await bootApplication(providers);
-  }
+  static Future<Nylo> nylo() async => await bootApplication(providers);
   static Future<void> finished(Nylo nylo) async => await bootFinished(nylo);
 }
