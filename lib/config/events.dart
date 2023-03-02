a16a2f51db903f9af24ea7a3a9f8eec4e8ab3619
Samed Ozdemir <samed@xsor.io>
2023-03-01 22:46:18 -0500

    fix: Remove login/logout event

--- lib/config/events.dart
+++ lib/config/events.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_app/app/events/register_event.dart';
 
-import '/app/events/login_event.dart';
-import '/app/events/logout_event.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 
 /*
@@ -15,7 +13,5 @@ import 'package:nylo_framework/nylo_framework.dart';
 */
 
 final Map<Type, NyEvent> events = {
-  LoginEvent: LoginEvent(),
-  LogoutEvent: LogoutEvent(),
   RegisterEvent: RegisterEvent(),
 };
