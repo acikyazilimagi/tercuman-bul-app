3f71029c0e7383dd197730ad2be4a897faaa2232
Samed Ozdemir <samed@xsor.io>
2023-03-04 23:18:30 -0500

    feat: Add uid to translator list item

--- lib/app/models/translator_list_item.dart
+++ lib/app/models/translator_list_item.dart
@@ -1,15 +1,18 @@
 class TranslatorListItem {
+  late String uid;
   late String name;
   late double distance;
   late List<String> languages;
 
   TranslatorListItem.fromJson(dynamic data) {
+    uid = data['uid'] ?? '';
     name = data['name'] ?? '';
     distance = data['distance'] ?? '';
     languages = List.from(data['languages'] ?? []);
   }
 
   toJson()  => {
+    "uid": uid,
     "name": name,
     "email": distance,
     "languages": languages,
